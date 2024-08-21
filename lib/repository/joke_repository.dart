cd2f22b8b9930dca3fa2541f836c3cc9a8553d4e
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-21 16:13:18 +0800

    feat: add model barrel file and temporary repository class
    - Created a barrel file for models
    - Implemented a temporary repository class

--- lib/repository/joke_repository.dart
+++ lib/repository/joke_repository.dart
@@ -1,3 +1,12 @@
-// class JokeRepository {
-//     final 
-// }
+import 'package:flutter_kokohub/repository/models/model_barrel.dart';
+import 'package:flutter_kokohub/service/joke_service.dart';
+
+class JokeRepository {
+    final JokeService service;
+
+    JokeRepository({
+        required this.service,
+    });
+
+    Future<Joke> getJokes() async => service.getJokes();
+}
